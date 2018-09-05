ccd416ab6c6d8a65882aa2ca246bf45064e74475
Wen Li <wenlicheong96@gmail.com>
2018-09-05 13:36:33 +0800

    version2

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,10 @@
+export const FIREBASE_CONFIG = 
+{
+    apiKey: "AIzaSyDoeCMmKWH_XWZpBbwT8smrmagshdXSWks",
+    authDomain: "testappauth-f7049.firebaseapp.com",
+    databaseURL: "https://testappauth-f7049.firebaseio.com",
+    projectId: "testappauth-f7049",
+    storageBucket: "testappauth-f7049.appspot.com",
+    messagingSenderId: "705597487350"
+  };
+ 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, NavParams, IonicPage, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+@IonicPage() 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private afAuth: AngularFireAuth, private toast: ToastController,
+    public navCtrl: NavController, public navParams: NavParams) {
   }
 
-}
+  ionViewWillLoad() {
+    this.afAuth.authState.subscribe(data => {
+      if (data && data.email && data.uid){
+      this.toast.create({
+        message: 'Welcome to APP_NAME,${data.email}',
+        duration:3000
+      }).present();
+    }
+    else{
+      this.toast.create({
+        message:'Could not find authentication details',
+        duration:3000
+      }).present();
+    }
+      
+   })
+  }
+}  
